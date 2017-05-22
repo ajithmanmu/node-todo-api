25e1ddcc981dd9b2e9dd77215574c8ddd5cab67f
ajith.manmadhan <ajith@surfline.com>
2017-05-22 14:56:05 -0700

    Setup app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/TodoApp');
-
+//mongoose.connect('mongodb://127.0.0.1:27017/TodoApp');
+//mongoose.connect('mongodb://ajithmanmu:todoapp_ajithmanmu@ds149221.mlab.com:49221/todoapp_ajithmanmu');
+mongoose.connect(process.env.MONGODB_URI|| 'mongodb://127.0.0.1:27017/TodoApp');
 module.exports = {mongoose};
