9d299626442e53f5f54b6a79dae6aba1dcce62fa
ajith.manmadhan <ajith@surfline.com>
2017-05-22 16:06:20 -0700

    Added Test cases for DELETE

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -97,6 +97,42 @@ describe('GET /todos/:id', ()=>{
     .expect(404)
     .end(done);
   });
+});
 
+describe('DELETE /todos/:id', ()=>{
+  it('should delete todo by ID', (done)=>{
+    var hexID = todos[0]._id.toHexString();
+    request(app)
+    .delete(`/todos/${hexID}`)
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todo._id).toBe(hexID);
+    })
+    .end((err, res)=>{
+      if(err){
+        return done(err);
+      }
+      Todo.findById(hexID).then((doc)=>{
+        expect(doc).toNotExist();
+        done();
+      }).catch((e)=>done(e));
+
+
+    });
+  });
+
+  it('should return 404 if todo not found', (done)=>{
+    request(app)
+    .delete(`/todos/${new ObjectID().toHexString()}`)
+    .expect(404)
+    .end(done);
+  });
+
+  it('should return 404 if objectID is invalid', (done)=>{
+    request(app)
+    .delete('/todos/123')
+    .expect(404)
+    .end(done);
+  });
 
 });
