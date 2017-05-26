17bf659f6dc345e572380d9c614f9127cc0c1179
ajith.manmadhan <ajith@surfline.com>
2017-05-25 17:17:22 -0700

    Add Tests for GET /users/me and POST /users

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,50 @@
+const {ObjectID} = require('mongodb');
+const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
+const jwt = require('jsonwebtoken');
+
+const userOneID = new ObjectID();
+const usertwoID = new ObjectID();
+const users = [{
+  _id: userOneID,
+  email: 'ajith@testing.com',
+  password:'test123',
+  tokens:[{
+    access:'auth',
+    token: jwt.sign({_id:userOneID,access:'auth'},'secret').toString()
+  }]
+},
+  {
+  _id:usertwoID,
+  email:'ajith2@testing.com',
+  password:'test456'
+  }
+]
+
+const todos = [{
+  _id: new ObjectID(),
+  text: 'First test todo'
+},{
+  _id: new ObjectID(),
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 3333
+}
+]
+
+//Wipes out the Mongo collection before the test is run and adds the dummy data above
+const populateTodos = (done) =>{
+  Todo.remove({}).then(()=> {
+    return Todo.insertMany(todos);
+  }).then(()=>done());
+}
+
+const populateUsers = (done) =>{
+  User.remove({}).then(()=>{
+    var userOne = new User(users[0]).save();
+    var userTwo = new User(users[1]).save();
+    return Promise.all([userOne,userTwo]);
+  }).then(()=>done());
+}
+
+module.exports = {todos, populateTodos, users, populateUsers};
